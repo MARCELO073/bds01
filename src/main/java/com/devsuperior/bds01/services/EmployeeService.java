0d3c662b057dd9dad7a714aecc5ecbda99421971
MARCELO073 <marcelonogueira072@gmail.com>
2021-11-01 13:50:14 -0300

    bds01 DepartmentControllerIT

---
+++ src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.devsuperior.bds01.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds01.repositorie.EmployeeRepository;
+@Service
+public class EmployeeService {
+	
+	EmployeeRepository repository;
+
+}
